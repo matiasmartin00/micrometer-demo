f38d3feb6501168d99259328040c33ede7ef56b3
matiasmartin <matiass.lean@gmail.com>
2025-11-22 19:42:13 +0100

    chore: init spring project

---
+++ src/test/java/com/mmartin/micrometer_demo/MicrometerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmartin.micrometer_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrometerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
